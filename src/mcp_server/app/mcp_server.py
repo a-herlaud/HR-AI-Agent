aaefa9e38b6fd39b5bfb4fb0c9ba224859ac8bb3
aherlaud <aherlaud@student.42.fr>
2026-08-02 01:06:19 +0200

    feat(mcp server): set up the mcp server container and use hr api tools
    - src/docker-compose.yml: add the mcp service communicating with api
    - src/mcp_server/Dockerfile: image for the mcp server launched via python sdk
    - src/mcp_server/app/mcp_server.py: use fasmcp sdk to run a server uvicorn in background with tools calling the hr api service
    - src/mcp_server/requirements.txt: dependencies for the mcp server

---
+++ src/mcp_server/app/mcp_server.py
@@ -0,0 +1,34 @@
+from fastmcp import FastMCP
+import requests
+
+mcp = FastMCP("HR MCP")
+
+@mcp.tool()
+def get_quarter_kpis() -> dict:
+    """
+    get the HR KPIs for the current quarter
+    """
+
+    response = requests.get(
+    "http://host.docker.internal:8000/api"
+    )
+
+    return reponse.json()
+
+@mcp.tool()
+def get_quarter_kpis(month: str) -> dict:
+    """
+    get the HR KPIs for a certain month
+    """
+
+    response = requests.get(
+    "http://host.docker.internal:8000/api/{month}"
+    )
+
+    return reponse.json()
+
+mcp.run(
+    transport="http",
+    host="0.0.0.0",
+    port=8001,
+)
